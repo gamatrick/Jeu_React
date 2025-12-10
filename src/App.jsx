6ca0198f51b3d9706eb6720907e4a6aca51ed17e
gamatrick <val@laflyfamily.com>
2025-12-10 11:20:23 +0100

    fonction Browser / menu jeu

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
-import Menu_jeu from './menu_jeu.jsx';
-import Jeu from './Jeu.jsx';
-import './App.css'
+import Menu_jeu from './Menu_jeu.jsx';
+import './App.css';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const navigate = useNavigate();
 
   return (
-    <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Menu jeu</h1>
+      <button onClick={() => navigate('/Jeu')}>Jouer</button>
+      <button onClick={() => navigate('/')}>Hight score</button>
+    </div>
+  );
 }
 
 function Main() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Menu_jeu />} />
-        <Route path="/Jeu" element={<App/>} />
-      </Routes>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Menu_jeu />} />  
+      <Route path="/Jeu" element={<App />} />    
+    </Routes>
+  );
 }
 
-export default App
-export { Main }
+export default App;
+export { Main };
